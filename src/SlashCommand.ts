2b4525b29a4e906456bd6aa2086637526db397a7
김도휘 <78037515+DOHWI@users.noreply.github.com>
2022-09-07 23:58:01 +0900

    Fix Type Error 🙂

--- src/SlashCommand.ts
+++ src/SlashCommand.ts
@@ -1,8 +1,8 @@
-import {CommandInteraction, SlashCommandBuilder, SlashCommandSubcommandsOnlyBuilder} from "discord.js";
+import {CommandInteraction, SlashCommandBuilder} from "discord.js";
 import Bot from "./Bot/Bot";
 
 type onInteractType = (bot: Bot, interaction: CommandInteraction) => void | Promise<void>;
 
 export default class SlashCommand {
-    constructor(public readonly slashCommand: SlashCommandBuilder | SlashCommandSubcommandsOnlyBuilder, public readonly onInteract: onInteractType) {}
+    constructor(public readonly slashCommand: Omit<SlashCommandBuilder, "addSubcommand" | "addSubcommandGroup">, public readonly onInteract: onInteractType) {}
 }
